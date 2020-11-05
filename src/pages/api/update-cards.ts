123d4165959a96d0ed2748f91f3e77f03a59e391
Patrick Dias <pdcamargo@hotmail.com>
2020-11-05 16:02:47 -0300

    housekeeping: underscore req param in api

--- src/pages/api/update-cards.ts
+++ src/pages/api/update-cards.ts
@@ -7,7 +7,7 @@ const route =
   'https://db.ygoprodeck.com/api/v7/cardinfo.php?offset=0&num=10000003';
 
 export default async function handler(
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse
 ) {
   const {
